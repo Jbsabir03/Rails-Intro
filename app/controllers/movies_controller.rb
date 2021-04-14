dded2858b0c66fbfb101de66d16077e11f64d954
Johnathan Sabir <694757@faulkner.edu>
2021-04-14 14:24:33 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,10 +7,12 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.with_ratings(ratings_list)
+    @movies = Movie.with_ratings(ratings_list, sort_by)
     @all_ratings = Movie.all_ratings
     @ratings_to_show = ratings_hash
-    byebug
+    @sort_by = sort_by
+    session[:sort_by] = sort_by
+    session[:ratings] = ratings_list
   end
 
   def new
@@ -41,15 +43,20 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
   
+  private
+  
+  def ratings_list
+    params[:ratings]&.keys || session[:ratings] || Movie.all_ratings
+  end
+  
   def ratings_hash
     Hash[ratings_list.collect {|item| [item,"1"]}]
   end
-
-  def ratings_list
-    params[:ratings]&.keys || Movie.all_ratings
+  
+  def sort_by
+    params[:sort_by] || session[:sort_by] || 'id'
   end
   
-  private
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
   def movie_params
